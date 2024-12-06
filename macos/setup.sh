851ad045b21bcdbb752313dc814960f3b916e6f4
Gilles Khouzam <gilles@khouzam.com>
2024-12-06 15:45:09 -0800

    Removing klogg and lastpass because they are rosetta

--- macos/setup.sh
+++ macos/setup.sh
@@ -41,14 +41,12 @@ brew_install git-gui
 brew_install sourcetree --cask
 brew_install iterm2 --cask
 brew_install visual-studio-code --cask
-brew_install lastpass --cask
 brew_install lastpass-cli
 brew_install iproute2mac
 brew_install monitorcontrol --cask
 brew_install android-platform-tools --cask
 brew_install flycut --cask
 brew_install iproute2mac
-brew_install klogg
 brew_install gh
 brew_install tmux
 brew_install htop
